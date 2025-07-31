82542e0331529b77bdbe39cefc2c134b36e0bb3a
mgjeffries <gib.jeffries@gmail.com>
2025-07-31 16:56:02 -0400

    fix where config button wasn't hooked up

--- network-monitor/network-monitor.ino
+++ network-monitor/network-monitor.ino
@@ -1,6 +1,8 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+#include <Pushbutton.h>
+
 
 // Heartbeat settings
 const char* heartbeatURL = "https://uptime.betterstack.com/api/v1/heartbeat/Byrp35GjYTtiTSb55ZFeQLJq";
@@ -8,7 +10,7 @@ const unsigned long interval = 5 * 60 * 1000;  // 5 minutes in ms
 unsigned long previousMillis = 0;
 
 // Config mode button
-#define CONFIG_BUTTON_PIN 35
+Pushbutton button(D9);
 
 
 void configModeCallback (WiFiManager *myWiFiManager) {
@@ -19,17 +21,20 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   }
 
 void setup() {
-  pinMode(CONFIG_BUTTON_PIN, INPUT);  // GPIO35 is input only (no pullup)
   delay(100);  // Let things stabilize
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP  
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected at setup!");
+  }
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  if ( digitalRead(CONFIG_BUTTON_PIN) == 1) {
+  if (button.getSingleDebouncedRelease())
+  {
     Serial.println("Config mode triggered by pin");
 
     WiFiManager wm;    
@@ -41,16 +46,14 @@ void loop() {
     wm.setConfigPortalTimeout(180);
     wm.setAPCallback(configModeCallback);
 
-    if (!wm.startConfigPortal("OnDemandAP")) {
+    if (!wm.startConfigPortal("floatyInternetMonitor")) {
       Serial.println("failed to connect and hit timeout");
       delay(3000);
       //reset and try again, or maybe put it to deep sleep
       ESP.restart();
       delay(5000);
     }
-
-    //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
+    Serial.println("connected...yay! :)");
 
   }
 
